47a71a95599f7e2a9a26eb028c9211a34de52d5e
Sergey Averin <122549207+sl1pperq@users.noreply.github.com>
2023-12-18 11:29:31 +0300

    Update itc_7_sameChar.cpp

--- itc_7_sameChar.cpp
+++ itc_7_sameChar.cpp
@@ -3,15 +3,15 @@
 #include <string>
 using namespace std;
 
-bool itc_isLetter(char ch) {
-    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '�' && ch <= '�') || (ch >= '�' && ch <= '�');
+bool isLetter(char ch) {
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= 'à' && ch <= 'ÿ') || (ch >= 'À' && ch <= 'ß');
 }
 
 
 char itc_sameChar(string str) {
     char result;
     for (int i = 0; i < find_len(str); i++) {
-        if ((str[i])) {
+        if (isLetter(str[i])) {
             for (int j = i + 1; j < find_len(str); j++) {
                 if (str[i] == str[j]) {
                     result = str[i];
